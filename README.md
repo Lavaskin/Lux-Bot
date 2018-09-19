51f5e11ca2533528eeb43b5a4b830b822954864d
Lavaskin <lavaskin@gmail.com>
2018-09-19 15:31:44 -0700

    Updated README.md and started on ~cleanchat command.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+<p align="center"><img src="https://imgur.com/a/7zCvgaD"></p>
+
 # LUX BOT
 
 This is just a silly Discord bot I'll work on in my spare time. No, you can't add it to your server, but you can clone my code!
--- main.js
+++ main.js
@@ -18,15 +18,33 @@ function sendBotMessage(bot, channelID, message) {
     });
 }
 
+// function botGetMessages(bot, channelID, numMessages) {
+//     bot.getMessages(channelID, null, null, numMessages, function(err, array) {
+//         if (!err){ 
+//             return array;
+//         } else {
+//             console.log("x " + err);
+//         }
+//     });
+// }
+
+// function botDeleteMessages(bot, channelID, messageIDs) {
+//     bot.deleteMessages(channelID, messageIDs, function(err) {
+//         if (err) {
+//             console.log("x " + err);
+//         }
+//     })
+// }
+
 function randomInsult() {
     return insults[Math.floor(Math.random() * insults.length)];
 }
 
 
 /* 
- * ==================================
+ * ==============================
  *           BOT SETUP
- * ================================== 
+ * ==============================
 */
 
 
@@ -49,15 +67,12 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     // if (userID === "425421488703668224" || userID === "164545170161926145") {
     //     sendBotMessage(bot, channelID, randomInsult());
     // }
-    // if (message === "eggplant") {
-    //     sendBotMessage(bot, channelID, ":eggplant:");
-    // }
 
     //handle cases where the prefix is used
     if (message[0].toString() === botPrefix) {
         message = message.substring(1);
 
-        //log user messages if not the bot
+        //log user messages if not the bot 
         if (userID != 491793054894653452) {
             console.log("x==================================x");
             console.log(" User: " + user + " [" + userID + "]");
@@ -91,6 +106,24 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     sendBotMessage(bot, channelID, "@" + user + ", you need at least two numbers to add.");
                 }
                 break;
+            case "cleanchat":
+                //only Lavaskin for now
+                // if (userID === "148907812670406656") {
+                //     if (args.length === 1) {
+                //         let numMessages = parseInt(args[0]);
+
+                //         if (typeof(numMessages) === "number") {
+                //             botDeleteMessages(botGetMessages(parseFloat(args[0]), channelID));
+                //         } else {
+                //             sendBotMessage(bot, channelID, "@" + user + ", The argument must contain numbers.");
+                //         }
+                //     } else {
+                //         botDeleteMessage(botGetMessages(1, channelID));
+                //     }
+                // } else {
+                //     sendBotMessage(bot, channelID, "@" + user + ", you don't have permission to use that command.");
+                // }
+                // break;
         }
     }
 });
