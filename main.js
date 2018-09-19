9de2531a6a81c3315f54d12e2a5cb96c40701ebb
Lavaskin <lavaskin@gmail.com>
2018-09-19 13:12:15 -0700

    Optimized commands and added argument infrastructure.

--- main.js
+++ main.js
@@ -36,20 +36,22 @@ var bot = new Discord.Client({
     autorun: true
 });
 
+//Bot turned on
 bot.on('ready', function (evt) {
     console.log(">I've been turned on!\n");
 });
 
+//Handling Messages
 bot.on('message', function (user, userID, channelID, message, evt) {
     message = message.toLowerCase();
 
     //handle generic messages
-    if (user === "stinky") {
-        sendBotMessage(bot, channelID, randomInsult());
-    }
-    if (message === "eggplant") {
-        sendBotMessage(bot, channelID, ":eggplant:");
-    }
+    // if (userID === "425421488703668224" || userID === "164545170161926145") {
+    //     sendBotMessage(bot, channelID, randomInsult());
+    // }
+    // if (message === "eggplant") {
+    //     sendBotMessage(bot, channelID, ":eggplant:");
+    // }
 
     //handle cases where the prefix is used
     if (message[0].toString() === botPrefix) {
@@ -64,8 +66,30 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             console.log("x==================================x");
         }
 
-        switch (message) {
-            case "insult": sendBotMessage(bot, channelID, randomInsult());
+        //[0] of messageArray is the command, everything else are arguments
+        let messageArray = message.split(" ");
+        let command = messageArray.shift();
+        let args = messageArray;
+
+        switch (command) {
+            case "insult":
+                sendBotMessage(bot, channelID, randomInsult());
+                break;
+            case "add":
+                if (args.length > 1) {
+                    let result = 0;
+
+                    for (let i = 0; i < args.length; i++) {
+                        args[i] = parseFloat(args[i]);
+
+                        if (typeof(args[i]) === "number")
+                            result += args[i];
+                    }
+                    
+                    sendBotMessage(bot, channelID, "@" + user + ", here's your result: " + result);
+                } else {
+                    sendBotMessage(bot, channelID, "@" + user + ", you need at least two numbers to add.");
+                }
                 break;
         }
     }
